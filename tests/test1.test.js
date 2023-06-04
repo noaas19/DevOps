b0545cd03f8247b3ebb2cc9d52647ca2b9aec208
Esty <estyblotnik@gmail.com>
2023-06-04 16:24:32 +0300

    test

--- tests/test1.test.js
+++ tests/test1.test.js
@@ -1,13 +1,74 @@
-const request = require('supertest')
-const app=require('../index')
+// Import necessary dependencies and modules
+const request = require('supertest');
+const mongoose = require('mongoose');
+const app = require('../index');
+const Student = require('../modules/student');
 
-describe("Test suite 1:",()=>{
-    test("test 1: ",async()=>{
-        const res = await request(app).get('/')
-        expect(res.statusCode).toEqual(200)
-    })
-    test("test 2: ",async()=>{
-        const res = await request(app).get('/1234')
-        expect(res.statusCode).toEqual(404)
-    })
-})
+// Connect to the test database before running the tests
+beforeAll(async () => {
+  const dbURL = 'mongodb+srv://devops:devops1234@devops.wbacnkc.mongodb.net/devops?retryWrites=true&w=majority';
+  await mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
+});
+
+// Disconnect from the test database after running the tests
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+
+// Clear the student collection before each test
+beforeEach(async () => {
+  await Student.deleteMany({});
+});
+
+// Test the POST /savestudent endpoint
+describe('POST /savestudent', () => {
+  test('should save a new student to the database', async () => {
+    const response = await request(app)
+      .post('/savestudent')
+      .send({
+        name: 'John Doe',
+        exam1: 90,
+        exam2: 85,
+        exam3: 95,
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('Student saved successfully!');
+
+    const savedStudent = await Student.findOne({ name: 'John Doe' });
+    expect(savedStudent).toBeTruthy();
+    expect(savedStudent.exam1).toBe(90);
+    expect(savedStudent.exam2).toBe(85);
+    expect(savedStudent.exam3).toBe(95);
+  });
+
+  test('should return an error if saving the student fails', async () => {
+    // Force a save error by providing an invalid student document
+    const response = await request(app)
+      .post('/savestudent')
+      .send({});
+
+    expect(response.status).toBe(500);
+    expect(response.text).toContain('Error saving student:');
+  });
+});
+
+// Test the GET / endpoint
+describe('GET /', () => {
+  test('should render the register view', async () => {
+    const response = await request(app).get('/');
+
+    expect(response.status).toBe(200);
+    expect(response.text).toContain('Register');
+  });
+});
+
+// Test the GET /test endpoint
+describe('GET /test', () => {
+  test('should return "Test hello"', async () => {
+    const response = await request(app).get('/test');
+
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Test hello');
+  });
+});
